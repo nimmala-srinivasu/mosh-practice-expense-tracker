f1201aa017b77983062641aa5f05543e5545064d
nimmala-srinivasu <srinivasu4321@gmail.com>
2023-03-30 19:05:26 +0530

    commit01

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import ExpenseFilter from "./components/ExpenseFilter";
+import ExpenseList from "./components/ExpenseList";
+
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "text a", amount: 10, category: "Utilities" },
+    { id: 2, description: "text b", amount: 30, category: "Utilities" },
+    { id: 3, description: "text c", amount: 45, category: "Utilities" },
+    { id: 4, description: "text d", amount: 7, category: "Entertainment" },
+  ]);
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+  console.log(expenses);
+  return (
+    <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+};
+
+export default App;
